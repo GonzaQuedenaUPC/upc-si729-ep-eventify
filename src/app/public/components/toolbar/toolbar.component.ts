095e7d64f94c815dc69bd62bcc45a4855c2c476f
GonzaQuedenaUPC <u202113656@upc.edu.pe>
2024-10-04 23:22:54 -0500

    style(public): add button label to navigation links.

--- src/app/public/components/toolbar/toolbar.component.ts
+++ src/app/public/components/toolbar/toolbar.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { LanguageSelectorComponent } from '../language-selector/language-selector.component';
+import {MatButton} from '@angular/material/button';
 
 @Component({
   selector: 'app-toolbar',
   standalone: true,
   imports: [
     MatToolbarModule,
-    LanguageSelectorComponent
+    LanguageSelectorComponent,
+    MatButton
   ],
   templateUrl: './toolbar.component.html',
   styleUrl: './toolbar.component.css'
