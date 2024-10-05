3306ef7928b869e89ac67ba3de8f84c5a4fb7204
GonzaQuedenaUPC <u202113656@upc.edu.pe>
2024-10-04 20:58:09 -0500

    feat(registration): add event summary entity.

---
+++ src/app/registration/models/event-summary.entity.ts
@@ -0,0 +1,30 @@
+class EventSummaryEntity {
+
+  private readonly _id: number = 0;
+  private readonly _name: string = '';
+  private readonly _description: string = '';
+  private readonly _scheduleAt: string = '';
+
+  constructor({ id=0, name='', description='', scheduleAt=''}) {
+    this._id = id;
+    this._name = name;
+    this._description = description;
+    this._scheduleAt = scheduleAt;
+  }
+
+  get id(): number {
+    return this._id;
+  }
+
+  get name(): string {
+    return this._name;
+  }
+
+  get description(): string {
+    return this._description;
+  }
+
+  get scheduleAt(): string {
+    return this._scheduleAt;
+  }
+}
